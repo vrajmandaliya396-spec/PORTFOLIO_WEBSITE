c1a4f888267fafa06c0259d46f52b8b413de8a98
vrajmandaliya396-spec <vrajmandaliya396@gmail.com>
2026-03-09 23:31:00 +0530

    re designing the project page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import  { useState } from "react";
 import Home from "./pages/Home";
 import { Route, Routes } from "react-router-dom";
 import Agence from "./pages/Agence";
--- src/components/projectsPage/ImgCard.jsx
+++ src/components/projectsPage/ImgCard.jsx
@@ -1,17 +1,10 @@
 import React from 'react'
 
-const ImgCard = (props) => {
+const ImgCard = () => {
   return (
-    <>
-     <div className="w-full h-130 flex">
-        <div className=" project1 shadow-2xl overflow-hidden  rounded-[50px] hover:shadow-3xl flex-1">
-            <img className='  hover:scale-105 h-full w-full' loading='lazy' src={props.img1} alt="" />
-        </div>
-        <div className="project2 shadow-2xl overflow-hidden rounded-[50px]  hover:shadow-3xl  flex-1">
-            <img className=' hover:scale-105 h-full w-full ' loading='lazy' src={props.img2} alt="" />
-        </div>
-      </div>
-      </>
+    <div>
+      
+    </div>
   )
 }
 
