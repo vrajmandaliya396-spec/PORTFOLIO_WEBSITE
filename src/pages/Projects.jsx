aa2b638ba9e8a53d1394f037f72d7d08df1f0cbe
vrajmandaliya396-spec <vrajmandaliya396@gmail.com>
2026-03-13 23:52:22 +0530

    vommiting the unreal comants

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -62,7 +62,7 @@ const Projects = () => {
       </div>
       <div className='-lg:mt-20 lol'>
         {projects.map(function (elem, idx) {
-          return <div key={idx} className='hero w-full lg:h-[475px] mb-4 flex lg:flex-row flex-col lg:gap-4 gap-2'>
+          return <div key={idx} className='hero w-full lg:h-118.75 mb-4 flex lg:flex-row flex-col lg:gap-4 gap-2'>
             <ImgCard image1={elem.image1} image2={elem.image2} />
           </div>
         })}
